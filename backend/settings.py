891592d4410d11fcfc647af93bd6ae583b1e70d0
Amao-J <fredjonathan22@yahoo.com>
2024-11-14 14:58:42 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://pina1-db.onrender.com/']
+
 
 CSRF_COOKIE_SECURE = False
 
@@ -172,7 +172,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','possible-gar-partially.ngrok-free.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','https://pina1-db.onrender.com']
 
 # Login and Logout Redirect
 LOGIN_REDIRECT_URL = "/"
