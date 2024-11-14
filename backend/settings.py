195b7b68ca4f815d07ed3bb6c3cd8eb78c3dad2f
Amao-J <fredjonathan22@yahoo.com>
2024-11-14 12:14:43 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
 ]
 
 
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000','http://localhost:3000','http://pina1-db.onrender.com']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -169,6 +169,7 @@ else:
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000/",
         "http://127.0.0.1:3000/",
+        "http://pina1-db.onrender.com"
     ]
 
 
@@ -180,7 +181,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','possible-gar-partially.ngrok-free.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','pina1-db.onrender.com']
 
 # Login and Logout Redirect
 LOGIN_REDIRECT_URL = "/"
